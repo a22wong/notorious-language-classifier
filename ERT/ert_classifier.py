f03d1d7ee76ceaf52068a7a4d5a0fc7eecb41e44
David Tamrazov <david.v.tamrazov@gmail.com>
2017-10-23 19:15:49 -0400

    Added contribution save the results discussion

--- ERT/ert_classifier.py
+++ ERT/ert_classifier.py
@@ -2,6 +2,7 @@ import numpy as np
 import re
 import matplotlib.pyplot as plt
 import seaborn
+import itertools
 
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_extraction.text import CountVectorizer 
@@ -19,7 +20,7 @@ def main():
 
 
     print("Reading testing data...")
-    
+
     # Testing data to record predictions 
     test_data = read_csv('/Users/Dave/Documents/Uni_Work/COMP_551/notorious-language-classifier/Data/test_set_x.csv')
     
@@ -91,10 +92,44 @@ def print_confusion_matrix(y_actual, y_pred):
     
     np.set_printoptions(precision=2)  
 
-    seaborn.heatmap(cnf_matrix)
+    plot_confusion_matrix(cnf_matrix, classes=['Slovak', 'French', 'Spanish', 'German', 'Polish'], normalize=True, title='Confusion Matrix')
     plt.show()
     plt.close(plt.figure())
     
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 def print_estimator_graph(validation_scores, training_scores, estimators):
     
@@ -156,7 +191,7 @@ def read_csv(filepath, labels=False):
             
             # counter += 1
             
-            # if (counter == 10000):
+            # if (counter == 1000):
             #     break
 
             # split the row 
